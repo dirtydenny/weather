0a2585a9c587cd0e1ca2052aaf300913c8206d8a
dirtydenny <dallen7@socal.rr.com>
2023-03-07 09:05:09 -0800

    bring files up to date

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+/* Global Variables
+ const currentTemp = 
+ const currentCondition (rain, snow, sunny, partly cloudy)
+*/
+
 $(document).ready(() => {
+  var boxForCurrentDay = document.querySelector("#currentday");
+  function currentDay(data, cityName) {
+    // var dateToday = moment(0).format("mm/dd/yyyy");
+    var cityNameEl = document.createElement("h2");
+    cityNameEl.textContent = cityName;
+    var imageEl = document.createElement("img");
+    var imageUrl =
+      "https://openweathermap.org/img/wn/" +
+      data.list[0].weather[0].icon +
+      ".png";
+      
+
+    imageEl.setAttribute("src", imageUrl);
+    var currentTempEl = document.createElement("p");
+    console.log(data.list[0].main.temp);
+    currentTempEl.textContent = "Tempurature: " + data.list[0].main.temp;
+    var humidityEl = document.createElement("p");
+    console.log(data.list[0].main.humidity);
+    currentHumidityEl.textContent ="Humidity: " + data.list[0].main.humidity;
+
+    boxForCurrentDay.append(cityNameEl);
+    boxForCurrentDay.append(currentTempEl);
+    boxForCurrentDay.append(imageUrl);
+    boxForCurrentDay.append(humidityEl);
+  }
   // ES6
   const fetchWeather = (search_term) => {
-    const API_KEY = "b2e24dab707e582bdf1f20a5720ab74e";
+    const API_KEY = "1450619e15ae8cf72330f2b72d1cc222";
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${search_term}&appid=${API_KEY}`
     )
@@ -15,12 +45,14 @@ $(document).ready(() => {
 
         console.log(coor);
         fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${coor.lat}&lon=${coor.lon}&appid=${API_KEY}`
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${coor.lat}&lon=${coor.lon}&appid=${API_KEY}&units=imperial`
         )
           .then((res) => res.json())
           .then((forecast_data) => {
             // TODO
             console.log(forecast_data);
+            currentDay(forecast_data, search_term);
+            //next5days(,)
           });
       });
   };
@@ -36,3 +68,33 @@ $(document).ready(() => {
   // input:  city name
   // output: weather of the city
 });
+
+function render() {
+  for (var i = 0; i < gifs.length; i++) {
+    var animUrl = gifs[i].images.original.url;
+    var stillUrl = gifs[i].images.original_still.url;
+    var gifTitle = gifs[i].title;
+
+    var gifElem = document.createElement("div");
+    gifElem.className = "col-12 col-md-4";
+    gifElem.innerHTML = `
+           <div class="card">
+            <img 
+              src="${stillUrl}"
+              data-anim="${animUrl}"
+              data-still="${stillUrl}" 
+              class="card-img-top gifImg" 
+              alt="${gifTitle}">
+            <div class="card-body">
+              <h5 class="card-title">${gifTitle}</h5>
+              <button 
+                class="btn btn-primary gifFav" 
+                data-index="${i}">
+                Favorite
+              </button>
+            </div>
+           </div>
+          `;
+    document.getElementById("gifDisp").append(gifElem);
+  }
+}
